627415d6a08882c0917c675841308c4892356111
J. Santos <jamillo@gmail.com>
2016-11-18 10:47:04 -0300

    Implements the interface of the servo abstraction

--- src/motors/servo.h
+++ src/motors/servo.h
@@ -6,17 +6,78 @@
 #ifndef WALKING_MOTORS_MOTOR_H
 #define WALKING_MOTORS_MOTOR_H
 
+#include <boost/optional.hpp>
+
 namespace mote
 {
 namespace walking
 {
 namespace motors
 {
+template <typename T>
+class Value
+{
+private:
+	boost::optional<T> writeValue;
+	boost::optional<T> readValue;
+public:
+	/**
+	 * Set the value for writting.
+	 *
+	 * @param value
+	 * @return
+	 */
+	Value& operator=(T value)
+	{
+		this->writeValue = value;
+		return *this;
+	}
+
+	/**
+	 * If there is a value to be written.
+	 * @return
+	 */
+	bool is_dirty()
+	{
+		return this->writeValue;
+	}
+
+	/**
+	 * Value read.
+	 *
+	 * @return
+	 */
+	const boost::optional<T> &read() const
+	{
+		return this->readValue;
+	}
+
+	/**
+	 * Set the read value.
+	 *
+	 * @param value
+	 */
+	void read(T value)
+	{
+		this->readValue = value;
+	}
+
+	/**
+	 * Clear the write value.
+	 */
+	void clear()
+	{
+		this->writeValue.reset();
+	}
+};
+
 class Servo
 {
 public:
-	double speed;
-	double angle;
+	Value<double> speed;
+	Value<double> angle;
+
+	virtual void apply() = 0;
 };
 }
 }
